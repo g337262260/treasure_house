9ca7c619f2e5103b121db857c64c66a1c9ff0d6e
guowei <guowei@han-sky.com>
2018-11-29 18:03:13 +0800

    “修改倒计时位置的问题

--- treasure_house/static/js/bg/particles.js
+++ treasure_house/static/js/bg/particles.js
@@ -1508,6 +1508,9 @@ window.particlesJS = function(tag_id, params){
   /* set size canvas */
   canvas_el.style.width = "100%";
   canvas_el.style.height = "100%";
+  canvas_el.style.position = "fixed";
+  canvas_el.style.top = "0";
+  canvas_el.style.zIndex = "-1";
 
   /* append canvas */
   var canvas = document.getElementById(tag_id).appendChild(canvas_el);
